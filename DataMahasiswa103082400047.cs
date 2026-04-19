4b0ac9dab9d9e153fd6f9cad35d5922d4bfcf8fb
DaffaRamadhan1220 <daffasurya212@gmail.com>
2026-04-19 23:03:24 +0700

    menambahkan class DataMahasiswa103082400047

---
+++ DataMahasiswa103082400047.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa103082400047
+{
+    public string Nama { get; set; }
+    public string Nim { get; set; }
+    public string Fakultas { get; set; }
+
+    public void ReadJSON()
+    {
+        string jsonFileName = "tp7_1_103082400047.json";
+
+        if (!File.Exists(jsonFileName))
+        {
+            Console.WriteLine($"File {jsonFileName} tidak ditemukan!");
+            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
+            return;
+        }
+
+        string jsonString = File.ReadAllText(jsonFileName);
+
+        var data = JsonSerializer.Deserialize<DataMahasiswa103082400047>(jsonString);
+
+        Console.WriteLine($"Nama {data.Nama} dengan nim {data.Nim} dari fakultas {data.Fakultas}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul7_kelompok_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103082400047 mahasiswa = new DataMahasiswa103082400047();
+            mahasiswa.ReadJSON();
+        }
+    }
+}
